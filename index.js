b22f6415ac2d1ca2009d02f7e95ecc92738c0048
Marquise <marzphillip@gmail.com>
2026-08-31 01:59:31 -0400

    figured multiple sounds to multiple buttons

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const pad1Btn = document.getElementById("p1");
 const pad2Btn = document.getElementById("p2");
-const audio = document.createElement("audio");
-audio.src = "./sounds/hat.mp3";
+// const audio = document.createElement("audio");
+// audio.src = "./sounds/hat.mp3";
+// creates an array of audios and assigns the src to each of them
+const srcArr = ['sounds/drum.MP3', 'sounds/hat.MP3'];
+const audios = new Array(srcArr.length).fill(new Audio());
+audios.forEach((audio, index) => audio.src = srcArr[index]);
+console.log(srcArr[0])
+console.log(srcArr[1])
 
 
 
-pad1Btn.addEventListener("click", playMusic)
-
-function playMusic(){
-    audio.play();
+pad1Btn.addEventListener("click", function () {
+    playSound("drum")
 }
+)
+
+pad2Btn.addEventListener("click", function () {
+    playSound("hat")
+}
+)
+
+// Source - https://stackoverflow.com/a/76196278
+// Posted by Roko C. Buljan, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-08-31, License - CC BY-SA 4.0
+
+const playSound = (fileName) => {
+  const audio = new Audio();
+  audio.src = `sounds/${fileName}.mp3`;
+  audio.play();
+};
 
