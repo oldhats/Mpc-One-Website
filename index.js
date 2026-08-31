d73c5e8b9ea218374268c0e303de70a00ff2c547
Marquise <marzphillip@gmail.com>
2026-08-30 23:23:49 -0400

    cant figure out how to add multiple sounds to different buttons

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const pad-1-btn = document.getElementById("p1");
+const pad1Btn = document.getElementById("p1");
+const pad2Btn = document.getElementById("p2");
+const audio = document.createElement("audio");
+audio.src = "./sounds/hat.mp3";
+
+
+
+pad1Btn.addEventListener("click", playMusic)
+
+function playMusic(){
+    audio.play();
+}
+
